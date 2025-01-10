c463442c25c977c2084dd09f207da9f46c27b7d4
dimka90 <dimkayilrit@gmail.com>
2025-01-10 15:29:47 +0100

    created multiple functions that handle different operations relating to mapping

--- Day1/map.go
+++ Day1/map.go
@@ -5,11 +5,29 @@ import "fmt"
 
 func main(){
 
-user :=make(map[string]int)
-
-user["dimka"] = 10
-
-user["Yilcash"]  = 20
-
+user := createnewMap()
+user1 := updateMap(user, "dimka", 10)
+user2 := updateMap(user1, "james",20)
+fmt.Println(user1)
 fmt.Println(user)
+fmt.Println(user2)
 }
+
+func  createnewMap() map[string]int {
+return make(map[string]int)
+}
+
+func updateMap(data map[string]int,key string, value int) map[string]int {
+// update map with the specific key
+data[key] = value
+return data
+}
+
+func deleteValue(data map[string]int, key string) {
+delete(data,key)
+
+}
+
+//func clearMapingField(data map[string]int) {
+//clear(data)
+//}
